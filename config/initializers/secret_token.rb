7257302945db44544c0073968aeb4b52638a737d
Matt Hwang <mhwang206@gmail.com>
2014-01-30 09:49:14 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Books::Application.config.secret_key_base = '14119902b2c7b0d2f1e43f4ec32b4a321b66be82007bba38b6800c6d34ebfb09fcb2671f77ac2989b6d13579b6fafe1da937161b92b41c61127714b3ba618ea2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Books::Application.config.session_store :cookie_store, key: '_books_session'
