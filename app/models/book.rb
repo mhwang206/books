83c0e344d24bac96b56a799838661f30aa72432a
Matt Hwang <mhwang206@gmail.com>
2014-01-30 10:20:34 -0800

    added books

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,6 +3,6 @@ class Book
   field :title, type: String
   field :author, type: String
   field :publication_year, type: Integer
-  field :ISBN, type: Integer
+  field :ISBN, type: String
   field :genre, type: String
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,9 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+
+books = Book.create([
+	{title: "The Hitchhiker's Guide To The Galaxy", author: "Douglas Adams", publication_year: "1979", ISBN: "0-330-25864-8", genre: "Science Fiction"},
+	{title: "Green Eggs And Ham", author: "Douglas Adams", publication_year: "1979", ISBN: "978-0-394-80016-5", genre: "Children's"},
+	{title: "Agile Development With Rails 4", author: "Sam Ruby", publication_year: "2013", ISBN: "978-1-93778-556-7", genre: "Computer Programming"}
+	])
